a47a72e4b2a917d44300559a20e4044c813bce1f
Nick Meldrum <nick@nickmeldrum.com>
2020-09-29 20:00:43 +0100

    refactor updateQuality method

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -10,6 +10,122 @@ export class Item {
   }
 }
 
+export class NormalItem {
+  private item: Item
+
+  constructor(from: Item) {
+    this.item = from
+  }
+
+  get name(): string {
+    return this.item.name
+  }
+  get sellIn(): number {
+    return this.item.sellIn
+  }
+  set sellIn(value: number) {
+    this.item.sellIn = value
+  }
+  get quality(): number {
+    return this.item.quality
+  }
+  set quality(value: number) {
+    this.item.quality = value
+  }
+
+  validate(): void {
+    this.validateQuality()
+    this.validateName()
+  }
+
+  protected validateQuality(): void {
+    if (this.quality < 0) {
+      throw new Error('quality must not be negative')
+    }
+    if (this.quality > 50) {
+      throw new Error('quality for normal items must not be more than 50')
+    }
+  }
+
+  protected validateName(): void {
+    if (this.name === '') {
+      throw new Error('must have a name')
+    }
+  }
+
+  updateQuality(): void {
+    const factor = this.quality <= 0 ? 0 : this.sellIn <= 0 ? 2 : 1
+    this.quality -= factor
+  }
+
+  updateSellIn(): void {
+    this.sellIn -= 1
+  }
+}
+
+export class LegendaryItem extends NormalItem {
+  constructor(from: Item) {
+    super(from)
+  }
+
+  validateQuality(): void {
+    if (this.quality < 0) {
+      throw new Error('quality must not be negative')
+    }
+    if (this.quality > 80) {
+      throw new Error('quality for legendary items must be 80')
+    }
+    if (this.quality < 80) {
+      throw new Error('quality for legendary items must be 80')
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  updateQuality(): void {}
+}
+
+export class AgedItem extends NormalItem {
+  constructor(from: Item) {
+    super(from)
+  }
+
+  updateQuality(): void {
+    const factor = this.quality === 50 ? 0 : this.sellIn <= 0 ? 2 : 1
+    this.quality += factor
+  }
+}
+
+export class BackstagePassItem extends NormalItem {
+  constructor(from: Item) {
+    super(from)
+  }
+
+  updateQuality(): void {
+    let factor = 1
+
+    if (this.sellIn > 5 && this.sellIn <= 10) factor = 2
+    if (this.sellIn > 0 && this.sellIn <= 5) factor = 3
+    if (this.sellIn <= 0) factor = -this.quality
+
+    this.quality += factor
+
+    if (this.quality > 50) this.quality = 50
+  }
+}
+
+const itemFactory = (from: Item): NormalItem => {
+  switch (from.name) {
+    case 'Sulfuras, Hand of Ragnaros':
+      return new LegendaryItem(from)
+    case 'Aged Brie':
+      return new AgedItem(from)
+    case 'Backstage passes to a TAFKAL80ETC concert':
+      return new BackstagePassItem(from)
+    default:
+      return new NormalItem(from)
+  }
+}
+
 export class GildedRose {
   items: Array<Item>
 
@@ -19,71 +135,11 @@ export class GildedRose {
 
   updateQuality(): Array<Item> {
     for (let i = 0; i < this.items.length; i++) {
-      const item: Item = this.items[i]
-
-      if (item.quality < 0) {
-        throw new Error('quality must not be negative')
-      }
-      if (item.quality > 50 && !item.name.startsWith('Sulfuras')) {
-        throw new Error('quality for normal items must not be more than 50')
-      }
-      if (item.quality > 80 && item.name.startsWith('Sulfuras')) {
-        throw new Error('quality for legendary items must be 80')
-      }
-      if (item.quality < 80 && item.name.startsWith('Sulfuras')) {
-        throw new Error('quality for legendary items must be 80')
-      }
-      if (this.items[i].name === '') {
-        throw new Error('must have a name')
-      }
-      if (
-        this.items[i].name != 'Aged Brie' &&
-        this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
+      const item: NormalItem = itemFactory(this.items[i])
+      item.validate()
+      item.updateQuality()
+      item.updateSellIn()
     }
-
     return this.items
   }
 }
